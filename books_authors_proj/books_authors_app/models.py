ac9f72d68603a0389d38380801efd307f89a6053
Derek Sparks <drsparks@gmail.com>
2021-03-10 10:38:42 -0800

    assignment complete

--- books_authors_proj/books_authors_app/models.py
+++ books_authors_proj/books_authors_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class books(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField(default = {title})
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return f"<Book: {self.title}>"
+
+
+class authors(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    books = models.ManyToManyField(books, related_name="authors")
+    notes = models.TextField(default = "A Book")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"<Author: {self.first_name} {self.last_name}>"
+
+
